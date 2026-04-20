7079329045975686febee87124aa2fdd7cda047f
Patrick <patrickrebecchi@hotmail.com>
2026-04-20 16:58:40 -0300

    Iniciando projeto

---
+++ src/main/java/com/patrick/animex/AnimexApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.animex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimexApplication.class, args);
+	}
+
+}
